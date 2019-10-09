4c850c725999f5f3e950e2f9cf406f7294e99c17
Wioletta Gluza <wiolagluza@gmail.com>
2019-10-09 15:06:41 +0200

    Updates

---
+++ precache-manifest.80d6e001ba728f38f71fe6faa4215500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792687d523ea852b3b99374e93392c81",
+    "url": "/socialshare/index.html"
+  },
+  {
+    "revision": "260d809b04b30e46dd2d",
+    "url": "/socialshare/static/css/main.b698738e.chunk.css"
+  },
+  {
+    "revision": "6a3420b18c4745133e07",
+    "url": "/socialshare/static/js/2.2a9f3241.chunk.js"
+  },
+  {
+    "revision": "260d809b04b30e46dd2d",
+    "url": "/socialshare/static/js/main.e93bfee5.chunk.js"
+  },
+  {
+    "revision": "3261af34e19ef12d2c23",
+    "url": "/socialshare/static/js/runtime-main.896ac25c.js"
+  }
+]);
